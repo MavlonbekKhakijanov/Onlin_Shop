befa331be2ae7916166f23fbaa8b62dcdf5d7f94
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-06-08 20:37:43 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const productList = document.querySelector("#product-list");
+const mostPopular = document.querySelector("#most-popular");
+
+const upDateUi = (data) => {
+  productList.innerHTML = "";
+  data.forEach((product) => {
+    const mostPopularTemplate = mostPopular.content.cloneNode(true);
+    const productTitle = mostPopularTemplate.querySelector(".product-title");
+    const productImg = mostPopularTemplate.querySelector(".product-img");
+    const myLink = mostPopularTemplate.querySelector(".my-link");
+    const productDescription = mostPopularTemplate.querySelector(
+      ".product-description"
+    );
+    productTitle.textContent = product.title;
+    productList.appendChild(mostPopularTemplate);
+  });
+
+  console.log(data);
+};
+
+getData(API).then((data) => {
+  upDateUi(data.products);
+});
