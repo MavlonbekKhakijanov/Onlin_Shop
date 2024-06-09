5ada3981689482f25fafe61f2741d29702975e62
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-06-09 19:31:04 +0300

    about changed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+import { getData } from "./request.js";
+const API = "https://dummyjson.com/products";
+console.log("vdf d");
 const productList = document.querySelector("#product-list");
 const mostPopular = document.querySelector("#most-popular");
 
 const upDateUi = (data) => {
   productList.innerHTML = "";
+
   data.forEach((product) => {
     const mostPopularTemplate = mostPopular.content.cloneNode(true);
     const productTitle = mostPopularTemplate.querySelector(".product-title");
@@ -11,6 +15,7 @@ const upDateUi = (data) => {
     const sale = mostPopularTemplate.querySelector(".skidka");
     const reyting = mostPopularTemplate.querySelector(".reyting");
     const oldPrice = mostPopularTemplate.querySelector(".old-price");
+    const link = mostPopularTemplate.querySelector("#link");
     const price = mostPopularTemplate.querySelector(".price");
     const productDescription = mostPopularTemplate.querySelector(
       ".product-description"
@@ -19,6 +24,7 @@ const upDateUi = (data) => {
     oldPrice.textContent = `${product.price}$`;
 
     sale.textContent = `${Math.floor(product.discountPercentage)}%`;
+
     price.textContent = `${(
       product.price -
       (product.price * product.discountPercentage) / 100
@@ -28,11 +34,11 @@ const upDateUi = (data) => {
 
     productImg.src = product.thumbnail;
     productList.appendChild(mostPopularTemplate);
+    link.href = `/pages/infoProduct.html?productID=${product.id}`;
   });
 
   console.log(data);
 };
-
 getData(API).then((data) => {
   upDateUi(data.products);
 });
--- js/request.js
+++ js/request.js
@@ -1,9 +1,7 @@
-const API = "https://dummyjson.com/products";
-
 const overlayEl = document.querySelector("#overlay");
 const loaderEl = document.querySelector("#loader");
 
-const getData = async (data) => {
+export const getData = async (data) => {
   const request = await fetch(data);
   const response = await request.json();
   overlayEl.classList.add("hidden");
